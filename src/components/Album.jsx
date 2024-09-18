05bf804a7af7bc44faa69288c2c961b64fea0042
Zairaxx <brandontsegai@gmail.com>
2024-09-18 10:26:31 +0200

    solution

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function Album({data}) {
+
+  let [show, setShow] = useState(false);
 
   let {name,tracks} = data;
 
-    return (
+  return (
       <div>
-        <h3>{name} <button>Hide</button></h3>
-        <ul>
+        {/* Alternativ 1 */}
+        {/* {show ? <h2>Nu ser du mig!</h2> : <h2>Nu är state = false</h2>} */}
+        {/* Alternativ 2 */}
+        {/* {show && <h2>Nu ser du mig!</h2>} */}
+
+        <h3>{name} <button onClick={() => {setShow(!show)}}>{!show ? "Show" : "Hide"}</button></h3>
+        {show && <ul>
           {tracks.map((track,i) => <li key={i}>{track}</li>)}
-        </ul>
+        </ul>}
       </div>
     );
   }
