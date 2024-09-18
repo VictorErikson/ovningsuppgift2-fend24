d90bc683a26302349d7fabd3fc524eaf15af119a
Zairaxx <brandontsegai@gmail.com>
2024-09-18 10:50:42 +0200

    added comments

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 export default function Album({data}) {
 
+  // Vi sätter ett state som heter show. Vi får bara ändra på vårt state med setShow-funktionen.
+  // Default värdet blir false.
   let [show, setShow] = useState(false);
 
   let {name,tracks} = data;
 
   return (
       <div>
-        {/* Alternativ 1 */}
-        {/* {show ? <h2>Nu ser du mig!</h2> : <h2>Nu är state = false</h2>} */}
-        {/* Alternativ 2 */}
-        {/* {show && <h2>Nu ser du mig!</h2>} */}
-
-        <h3>{name} <button onClick={() => {setShow(!show)}}>{!show ? "Show" : "Hide"}</button></h3>
+        <h3>{name}
+          {/* Vi använder setShow(!show) för att toggla värdet true/false i vår onClick*/}
+          {/* Vi använder även Ternary operators dvs ? : för att anpassa texten baserat på state */}
+          <button onClick={() => {setShow(!show)}}>{!show ? "Show" : "Hide"}</button>
+        </h3>
+      {/* Vi använder && för att säga: Om show är true, returnera <ul> */}
         {show && <ul>
           {tracks.map((track,i) => <li key={i}>{track}</li>)}
         </ul>}
