8651351a0a13e07d9586d464a4f8fa30d47d9ce2
Zairaxx <brandontsegai@gmail.com>
2025-08-20 13:04:58 +0200

    added css

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,7 +1,7 @@
 export default function Album() {
     return (
-      <div>
-        Album namn {/* Byt ut mot en variabel. */}
+      <div className="Album">
+        <h3 className="Album-title">Album namn</h3> {/* Byt ut mot en variabel. */}
         <ul>
           <li>Låt 1</li>
           <li>Låt 2</li>
